923c15cc3e0a426684c66e99369ababc4268122f
masuhara <info@masuhara.net>
2016-01-10 16:41:12 +0900

    Initial Commit

--- NSTableViewSampler/ViewController.swift
+++ NSTableViewSampler/ViewController.swift
@@ -8,12 +8,14 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    var contentArray = ["はしお", "きのぽん", "ともひろ", "もりだい", "Dのすけ", "かなみん", "ゲバちゃん"]
+    @IBOutlet weak var table: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.layer?.backgroundColor = NSColor.whiteColor().CGColor
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +23,25 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    // MARK: - NSTableView DataSource
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return contentArray.count
+    }
 
-
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        return contentArray[row]
+    }
+    
+    // MARK: - NSTableView Delegate
+    func tableView(tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        print(row)
+        let alert = NSAlert()
+        alert.messageText = "Selected"
+        alert.informativeText = contentArray[row] + "が選択されました"
+        alert.addButtonWithTitle("OK")
+        alert.runModal()
+        return true
+    }
 }
 
