c43b2a4b97923d8cbda8af04055fdb8cd7c94cfb
masuhara <info@masuhara.net>
2016-01-10 14:16:55 +0900

    Initial Commit

---
+++ NSTableViewSampler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTableViewSampler
+//
+//  Created by Masuhara on 2016/01/10.
+//  Copyright © 2016年 Daisuke Masuhara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTableViewSampler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTableViewSampler
+//
+//  Created by Masuhara on 2016/01/10.
+//  Copyright © 2016年 Daisuke Masuhara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
